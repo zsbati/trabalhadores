7c06a840717c6ca195ab6f268b8e99df77cbbe34
zsbati <zbati123@gmail.com>
2025-05-27 23:03:05 +0100

    su

--- teachers_app/management/commands/create_superuser_non_interactive.py
+++ teachers_app/management/commands/create_superuser_non_interactive.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 import os
 
 class Command(BaseCommand):
@@ -11,7 +12,13 @@ class Command(BaseCommand):
         password = os.getenv('SUPERUSER_PASSWORD', 'admin123')
 
         if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username, email, password)
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password,
+                is_staff=True,
+                is_superuser=True
+            )
             self.stdout.write(self.style.SUCCESS(f'Successfully created superuser {username}'))
         else:
             self.stdout.write(self.style.WARNING(f'Superuser {username} already exists'))
